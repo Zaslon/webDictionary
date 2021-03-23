604c121ef5b0470f47f7ef63916916aec2f8c5c6
Zaslon <mafi553@gmail.com>
2021-03-23 23:51:36 +0900

    ズーム対応

--- dict.php
+++ dict.php
@@ -68,7 +68,7 @@ $json = json_decode($json,true);
 <?php
 	require 'anal.php';
 ?>
-<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0,user-scalable=no" />
+<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=4.0,minimum-scale=1.0,user-scalable=yes" />
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <meta http-equiv="Content-Script-Type" content="text/javascript" />
 <meta http-equiv="Content-Style-Type" content="text/css" /> 
