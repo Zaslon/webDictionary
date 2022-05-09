1656ee70bc56d4cf24fcca23a68b25c2bf6609db
Zaslon <mafi553@gmail.com>
2022-05-10 00:42:44 +0900

    change font, change font weight and size

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function changeFont(){
 	let list = document.getElementsByClassName('wordForm');
 	let font = 'inherit';
-	let size = '150%';
+//	let size = '150%';
 	if (document.getElementById('c5').checked) {
-		font = 'Fazik';
-		size = '170%';
+		font = 'Endrata';
+//		size = '170%';
 	}
 	for (let i = 0; i < list.length; ++i) {
 		list[i].style.fontFamily= font;
-		list[i].style.fontSize= size;
+//		list[i].style.fontSize= size;
 	}
 }
 
